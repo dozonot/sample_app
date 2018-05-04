85720551e1cd3ae5339aa1c9e3105e8b2de56cae
Dozono <dozono@dozono.net>
2018-05-05 03:11:33 +0900

    add show user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
+    # => app/views/users/show.html.erb
+    #debugger
+  end
+
   def new
   end
 end
