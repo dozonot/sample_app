83d423905d29c4c8cb1704d4e976e9f9b900fbdc
Dozono <dozono@dozono.net>
2018-05-05 05:23:38 +0900

    modify gravatar. todo:publish image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     # => app/views/users/show.html.erb
-    #debugger
+    # debugger
   end
 
   def new
