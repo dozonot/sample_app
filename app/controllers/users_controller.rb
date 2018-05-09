788feee6accec880c41adef9761ff9df7382eafc
Dozono <dozono@dozono.net>
2018-05-09 21:38:40 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,13 +11,22 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
-
+    @user = User.new(user_params)
     if @user.save
       # Sucess
+      # redirect_to @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to user_url(@user)
     else
       # Failure
-      rendre 'new'
+      render 'new'
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit( :name, :email, :password,
+                                  :password_confirmation)
   end
 end
