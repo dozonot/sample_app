760ab2be69f14d8b9ff888b88a03a809676c4572
Dozono <dozono@dozono.net>
2018-05-11 15:21:24 +0900

    finish 8.1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
+  # GET /login
   def new
+  end
+
+  # POST /login
+  def create
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      # Success
+    else
+      # Failure email or password not collect
+      flash.now[:danger] = 'Invalied email/password combination'
+      render 'new'
+    end
+  end
+
+  # DELETE /logout
+  def destroy
   end
 end
