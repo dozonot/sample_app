26f78339b4593084cf7b314a54eace5ea72e06d9
Dozono <dozono@dozono.net>
2018-05-23 00:23:38 +0900

    finish 11.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,10 +23,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Sucess
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to user_url(@user)
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
+      # log_in @user
+      # flash[:success] = "Welcome to the Sample App!"
+      # redirect_to user_url(@user)
     else
       # Failure
       render 'new'
